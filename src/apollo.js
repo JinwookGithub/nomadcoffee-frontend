25fdc9873b8fc3ee7c7ba76e9b52ad7e59018ad0
jinwook jung <jinwook@pop-os.localdomain>
2021-10-13 04:39:31 +0900

    [A08] Admin Login

--- src/apollo.js
+++ src/apollo.js
@@ -1,4 +1,24 @@
-import { makeVar } from "@apollo/client";
+import routes from "./routes";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 
-export const isLoggedInVar = makeVar(false);
+const TOKEN = "token";
+
+export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
+
+export const logUserIn = (token) => {
+	localStorage.setItem(TOKEN, token);
+	isLoggedInVar(true);
+};
+
+export const logUserOut = history => {
+  localStorage.removeItem(TOKEN);
+  history?.replace(routes.home, null);
+  window.location.reload();
+};
+
 export const darkModeVar = makeVar(false);
+
+export const client = new ApolloClient({
+	uri: "http://localhost:5000/graphql",
+	cache: new InMemoryCache(),
+});
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,24 +1,139 @@
+import { gql, useMutation } from "@apollo/client";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
-import { darkModeVar } from "../apollo";
+import { logUserIn } from "../apollo";
+import AuthLayout from "../components/auth/AuthLayout";
+import BottomBox from "../components/auth/BottomBox";
+import Button from "../components/auth/Button";
+import FormBox from "../components/auth/FormBox";
+import FormError from "../components/auth/FormError";
+import Input from "../components/auth/Input";
+import Separator from "../components/auth/Separator";
+import PageTitle from "../components/PageTitle";
+import routes from "../routes";
 
-const Title = styled.h1`
-  color: ${props => props.theme.fontColor};
+const FacebookLogin = styled.div`
+	color: #385285;
+	span {
+		margin-left: 10px;
+		font-weight: 600;
+	}
 `;
 
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
+const Notification = styled.div`
+	color: #2ecc71;
+`;
+
+const LOGIN_MUTATION = gql`
+	mutation login($username: String!, $password: String!) {
+		login(username: $username, password: $password) {
+			ok
+			token
+			error
+		}
+	}
 `;
 
 function Login() {
-  return (
-    <Container>
-      <Title>Login</Title>
-      <button onClick={() => darkModeVar(true)}>To dark</button>
-      <button onClick={() => darkModeVar(false)}>To light</button>
-    </Container>
-  );
+	const location = useLocation();
+	//console.log(location);
+	const {
+		register,
+		handleSubmit,
+		errors,
+		formState,
+		getValues,
+		setError,
+		clearErrors,
+	} = useForm({
+		mode: "onChange",
+		defaultValues: {
+			username: location?.state?.username || "",
+			password: location?.state?.password || "",
+		},
+	});
+	const onCompleted = (data) => {
+		const {
+			login: { ok, error, token },
+		} = data;
+		if (!ok) {
+			return setError("result", {
+				message: error,
+			});
+		}
+		if (token) {
+			logUserIn(token);
+		}
+	};
+	const [login, { loading }] = useMutation(LOGIN_MUTATION, {
+		onCompleted,
+	});
+	const onSubmitValid = (data) => {
+		if (loading) {
+			return;
+		}
+		const { username, password } = getValues();
+		login({
+			variables: { username, password },
+		});
+	};
+	const clearLoginError = () => {
+		clearErrors("result");
+	};
+	return (
+		<AuthLayout>
+			<PageTitle title="Login" />
+			<FormBox>
+				<div>
+					<FontAwesomeIcon icon={faCoffee} size="3x" />
+				</div>
+				<Notification>{location?.state?.message}</Notification>
+				<form onSubmit={handleSubmit(onSubmitValid)}>
+					<Input
+						{...register("username", {
+							required: "Username is required",
+							minLength: {
+								value: 5,
+								message: "Username should be longer than 5 chars.",
+							},
+							/*
+              pattern: "",
+              validate: currentValue => currentValue.includes("potato"),
+              */
+						})}
+						onFocus={clearLoginError}
+						type="text"
+						placeholder="Username"
+						hasError={Boolean(formState.errors?.username?.message)}
+					/>
+					<FormError message={formState.errors?.username?.message} />
+					<Input
+						{...register("password", {
+							required: "Password is required.",
+						})}
+						onFocus={clearLoginError}
+						type="password"
+						placeholder="Password"
+						hasError={Boolean(formState.errors?.username?.message)}
+					/>
+					<FormError message={formState.errors?.password?.message} />
+					<Button
+						type="submit"
+						value={loading ? "Loading..." : "Log in"}
+						disabled={!formState.isValid || loading}
+					/>
+					<FormError message={formState.errors?.result?.message} />
+				</form>
+			</FormBox>
+			<BottomBox
+				cta="Don't have an account?"
+				linkText="Sign up"
+				link={routes.signUp}
+			/>
+		</AuthLayout>
+	);
 }
-/*
-const Login = () => <h1>Login</h1>;
-*/
 export default Login;
